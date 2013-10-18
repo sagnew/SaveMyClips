e5f41309632b75785d67b40a9be02a3b2092c63f
Sam Agnew <sagnew@eden.rutgers.edu>
2013-10-18 18:29:17 -0400

    Updates current copied index as well

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
 var clipboardList = [];
+var currentIndex;
 
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
         if (request.event == "copy") {
             //Handle the clipboard data here
             clipboardList.push(Clipboard.paste());
+            currentIndex = clipboadList.length - 1;
         }
         sendResponse({});
 });

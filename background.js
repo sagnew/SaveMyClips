7b5b889c9f9a86c73645da9d944fb60739613d34
Sam Agnew <sagnew@eden.rutgers.edu>
2013-10-18 18:18:25 -0400

    Keeps track of clipboard history

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
+var clipboardList = [];
+
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
         if (request.event == "copy") {
             //Handle the clipboard data here
+            clipboardList.push(Clipboard.paste());
         }
         sendResponse({});
 });

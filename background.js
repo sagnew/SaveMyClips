55f44fee30de04724e62ac273d96f124fd9b96b0
Sam Agnew <sagnew@eden.rutgers.edu>
2013-10-19 04:17:14 -0400

    Fixed a minor assignment error

--- background.js
+++ background.js
@@ -1,22 +1,21 @@
 var clipboardList = [];
-var currentIndex;
+var currentIndex = 0;
 
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
         if (request.event == "copy") {
-            //Handle the clipboard data here
             clipboardList.push(Clipboard.paste());
-            currentIndex = clipboadList.length - 1;
+            currentIndex = clipboardList.length - 1;
         }
         sendResponse({});
 });
 
 chrome.commands.onCommand.addListener(function(command) {
     if(command === "back") {
-       currentIndex -= 1;
-       Clipboard.copy(clipboardList[currentIndex]);
+        currentIndex -= 1;
+        Clipboard.copy(clipboardList[currentIndex]);
     }else if(command === "forward") {
-       currentIndex += 1;
-       Clipboard.copy(clipboardList[currentIndex]);
+        currentIndex += 1;
+        Clipboard.copy(clipboardList[currentIndex]);
     }
 });

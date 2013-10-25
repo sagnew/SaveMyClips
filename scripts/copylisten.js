a86a8fc5a8efb71bb32470c3712287c0640a7c86
Sam Agnew <sagnew@eden.rutgers.edu>
2013-10-25 16:27:13 -0400

    Added a listener for cut events

--- scripts/copylisten.js
+++ scripts/copylisten.js
@@ -2,4 +2,10 @@ function onCopy(e) {
     chrome.extension.sendRequest({event: "copy"});
 }
 
-document.addEventListener("copy",onCopy,true);
+document.addEventListener("copy", onCopy, true);
+
+function onCut(e) {
+    chrome.extension.sendRequest({event: "copy"});
+}
+
+document.addEventListener("cut", onCut, true);
